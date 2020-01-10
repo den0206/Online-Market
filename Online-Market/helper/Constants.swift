3228c577223b2b1291d695b9b6620f562f5a280e
den0206 <mac.pop@mineo.jp>
2020-01-10 22:11:13 +0900

    constracts

---
+++ Online-Market/helper/Constants.swift
@@ -0,0 +1,48 @@
+//
+//  Constants.swift
+//  Online-Market
+//
+//  Created by 酒井ゆうき on 2020/01/06.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import Foundation
+
+// IDS & KEYS
+public let kFILEREFERENCE = "gs://market-c8b09.appspot.com"
+
+// Firebase Headers
+public let kUSER_PATH = "User"
+public let kCATEGORY_PATH = "Category"
+public let kITEMS_PATH = "Items"
+public let kBASKET_PATH = "Basket"
+
+// Category
+
+public let kNAME = "name"
+public let kIMAGENAME = "imageName"
+public let kOBJECTID = "objectId"
+
+// Items
+
+public let kCATEGORYID = "categoryId"
+public let kDESCRIPTION = "description"
+public let kPRICE = "price"
+public let kIMAGELINKS = "imageLinks"
+
+// Cart
+
+public let kOWNERID = "ownerId"
+public let kITEMSIDS = "itemsIds"
+
+// User
+
+public let kEMAIL = "email"
+public let kFIRSTNAME = "firstName"
+public let kLASTNAME = "lastName"
+public let kFULLNAME = "fullName"
+public let kCURRENTUSER = "currentUser"
+public let kFULLADRESS = "fullAdress"
+public let kONBOARD = "onBoard"
+public let kPURCHASEDITEMIDS = "purchasedIds"
+ 
